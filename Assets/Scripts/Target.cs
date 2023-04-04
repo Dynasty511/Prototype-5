f3e754182c6b0302e9643a33c47a04f0d84a2734
Dynasty511 <jukang@s.sfusd.edu>
2023-04-04 14:11:08 -0700

    added game manager script

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -17,7 +17,7 @@ public class Target : MonoBehaviour
         targetRb = GetComponent<Rigidbody>();
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
-        targetRb.AddTorque(RandomTorque(), RandomTorque, RandomTorque(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
 
         transform.position = RandomSpawnPos();
     }
