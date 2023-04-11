903e5c3443f0574b42a2d08b34eff5b1ec9240ea
Dynasty511 <jukang@s.sfusd.edu>
2023-04-11 13:53:12 -0700

    added script to prefabs to repeat spawn

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -7,10 +7,10 @@ public class Target : MonoBehaviour
     private Rigidbody targetRb;
     private GameManager gameManager;
     private float minSpeed = 12;
-    private float maxSpeed = 16;
+    private float maxSpeed = 13;
     private float maxTorque = 10;
     private float xRange = 4;
-    private float ySpawnPos = -6;
+    private float ySpawnPos = -2;
 
     public int pointValue;
     // Start is called before the first frame update
